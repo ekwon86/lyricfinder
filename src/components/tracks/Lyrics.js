79cd27b8209453224464f1821011c8ca0ad28b46
Eugene Kwon <ekwon86@gmail.com>
2018-11-13 08:08:20 -0800

    finished lyrics page

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import Moment from "react-moment";
 
 class Lyrics extends Component {
   state = {
@@ -31,11 +34,53 @@ class Lyrics extends Component {
 
   render() {
     const { track, lyrics } = this.state;
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    );
+
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>:{" "}
+              {track.primary_genres.music_genre_list.length === 0
+                ? "N/A"
+                : track.primary_genres.music_genre_list[0].music_genre
+                    .music_genre_name}
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>:
+              {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>:{" "}
+              <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
+            </li>
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 export default Lyrics;
